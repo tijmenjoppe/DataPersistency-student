e205324b9f71ac5e9765840349927070f316d6ab
Tijmen Muller <tijmen.muller@hu.nl>
2020-05-13 20:33:41 +0200

    Added Main class to test Hibernate annotations.

---
+++ practicum/OV-chipkaart_Hibernate/src/nl/hu/dp/ovchip/Main.java
@@ -0,0 +1,72 @@
+package nl.hu.dp.ovchip;
+
+import nl.hu.dp.ovchip.domein.Adres;
+import nl.hu.dp.ovchip.domein.OVChipkaart;
+import nl.hu.dp.ovchip.domein.Product;
+import nl.hu.dp.ovchip.domein.Reiziger;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import javax.persistence.metamodel.EntityType;
+import javax.persistence.metamodel.Metamodel;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * Testklasse - deze klasse test alle andere klassen in deze package.
+ *
+ * System.out.println() is alleen in deze klasse toegestaan (behalve voor exceptions).
+ *
+ * @author tijmen.muller@hu.nl
+ */
+public class Main {
+    // Creëer een factory voor Hibernate sessions.
+    private static final SessionFactory factory;
+
+    static {
+        try {
+            // Create a Hibernate session factory
+            factory = new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    /**
+     * Retouneer een Hibernate session.
+     *
+     * @return Hibernate session
+     * @throws HibernateException
+     */
+    private static Session getSession() throws HibernateException {
+        return factory.openSession();
+    }
+
+    public static void main(String[] args) throws SQLException {
+        testFetchAll();
+    }
+
+    /**
+     * P6. Haal alle (geannoteerde) entiteiten uit de database.
+     */
+    private static void testFetchAll() {
+        Session session = getSession();
+        try {
+            Metamodel metamodel = session.getSessionFactory().getMetamodel();
+            for (EntityType<?> entityType : metamodel.getEntities()) {
+                Query query = session.createQuery("from " + entityType.getName());
+
+                System.out.println("[Test] Alle objecten van type " + entityType.getName() + " uit database:");
+                for (Object o : query.list()) {
+                    System.out.println("  " + o);
+                }
+                System.out.println();
+            }
+        } finally {
+            session.close();
+        }
+    }
+}
